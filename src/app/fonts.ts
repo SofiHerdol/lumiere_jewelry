2bd8eef2852de81c6f977e94a00d8fa8e2eb580e
Sofía Heredia <sofiaherdol@gmail.com>
2024-06-03 22:18:11 -0300

    Lumière Initial Commit

---
+++ src/app/fonts.ts
@@ -0,0 +1,5 @@
+import { Cinzel } from "next/font/google";
+import { Cinzel_Decorative } from "next/font/google";
+
+export const cinzel = Cinzel ({ subsets: ["latin"], weight: "400" })
+export const cinzel_decorative = Cinzel_Decorative ({ subsets: ["latin"], weight: "400" })
